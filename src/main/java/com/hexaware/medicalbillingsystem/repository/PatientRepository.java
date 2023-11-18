393ba6a32a089cc5c9ed39e679997bfa9ca6d6cf
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 12:08:18 +0530

    Update PatientRepository.java

--- src/main/java/com/hexaware/medicalbillingsystem/repository/PatientRepository.java
+++ src/main/java/com/hexaware/medicalbillingsystem/repository/PatientRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hexaware.medicalbillingsystem.entities.Patients;
-
+/*
+@Author :   Rajat Darvhekar  
+Modified Date :06-11-2023
+Description : Repository for Patients class extending JpaRepository
+*/
 @Repository
 public interface PatientRepository extends JpaRepository<Patients, Long> {
 	
