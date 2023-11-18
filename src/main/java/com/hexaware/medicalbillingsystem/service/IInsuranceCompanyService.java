23c7c5be7b4057a58ddfe7afc46288b76c7090af
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 12:10:47 +0530

    Update IInsuranceCompanyService.java

--- src/main/java/com/hexaware/medicalbillingsystem/service/IInsuranceCompanyService.java
+++ src/main/java/com/hexaware/medicalbillingsystem/service/IInsuranceCompanyService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import com.hexaware.medicalbillingsystem.dto.InsuranceCompanyDTO;
 import com.hexaware.medicalbillingsystem.entities.InsuranceCompany;
-
+/*
+@Author :  Hema Sree  
+Modified Date : 04-11-2023
+Description : Interface of  IInsuranceCompanyService performing the following operations
+*/
 public interface IInsuranceCompanyService {
 	public InsuranceCompany addCompany(InsuranceCompanyDTO companydto);
 
