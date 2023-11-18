bcce46276868ad23cda4cb78286b82c8710c37f8
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 11:32:16 +0530

    Update InvoiceDetailsDTO.java

--- src/main/java/com/hexaware/medicalbillingsystem/dto/InvoiceDetailsDTO.java
+++ src/main/java/com/hexaware/medicalbillingsystem/dto/InvoiceDetailsDTO.java
@@ -11,53 +11,62 @@ public class InvoiceDetailsDTO {
 	private int invoiceTax;
 	private double invoiceTotalAmount;
 	private Patients patient;
+	private double consultationFee;
+	private double dianosticTestsFeee;
+	private double diagnosticScanFee;
 
 	public int getInvoiceId() {
 		return invoiceId;
 	}
-
 	public void setInvoiceId(int invoiceId) {
 		this.invoiceId = invoiceId;
 	}
-
 	public LocalDate getInvoiceDate() {
 		return invoiceDate;
 	}
-
 	public void setInvoiceDate(LocalDate invoiceDate) {
 		this.invoiceDate = invoiceDate;
 	}
-
 	public LocalDate getInvoicedueDate() {
 		return invoicedueDate;
 	}
-
 	public void setInvoicedueDate(LocalDate invoicedueDate) {
 		this.invoicedueDate = invoicedueDate;
 	}
-
-	public Patients getPatient() {
-		return patient;
-	}
-
-	public void setPatient(Patients patient) {
-		this.patient = patient;
-	}
-
 	public int getInvoiceTax() {
 		return invoiceTax;
 	}
-
 	public void setInvoiceTax(int invoiceTax) {
 		this.invoiceTax = invoiceTax;
 	}
-
 	public double getInvoiceTotalAmount() {
 		return invoiceTotalAmount;
 	}
-
 	public void setInvoiceTotalAmount(double invoiceTotalAmount) {
 		this.invoiceTotalAmount = invoiceTotalAmount;
 	}
-
+	public Patients getPatient() {
+		return patient;
+	}
+	public void setPatient(Patients patient) {
+		this.patient = patient;
+	}
+	public double getConsultationFee() {
+		return consultationFee;
+	}
+	public void setConsultationFee(double consultationFee) {
+		this.consultationFee = consultationFee;
+	}
+	public double getDianosticTestsFeee() {
+		return dianosticTestsFeee;
+	}
+	public void setDianosticTestsFeee(double dianosticTestsFeee) {
+		this.dianosticTestsFeee = dianosticTestsFeee;
+	}
+	public double getDiagnosticScanFee() {
+		return diagnosticScanFee;
+	}
+	public void setDiagnosticScanFee(double diagnosticScanFee) {
+		this.diagnosticScanFee = diagnosticScanFee;
+	}
 }
