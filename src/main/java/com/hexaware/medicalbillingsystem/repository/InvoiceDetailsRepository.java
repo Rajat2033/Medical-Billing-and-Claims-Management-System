754d998ebd963200cfe132910c6ffe0d080b1aeb
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 12:07:54 +0530

    Update InvoiceDetailsRepository.java

--- src/main/java/com/hexaware/medicalbillingsystem/repository/InvoiceDetailsRepository.java
+++ src/main/java/com/hexaware/medicalbillingsystem/repository/InvoiceDetailsRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hexaware.medicalbillingsystem.entities.InvoiceDetails;
-
+/*
+@Author :   Rajat Darvhekar  
+Modified Date :06-11-2023
+Description : Repository for InvoiceDetails class extending JpaRepository
+*/
 @Repository
 public interface InvoiceDetailsRepository extends JpaRepository<InvoiceDetails, Integer> {
 
