942b0d564f83460ad19f3ff5f13bf4c0946818bd
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 11:59:40 +0530

    Update ClaimNotValidException.java

--- src/main/java/com/hexaware/medicalbillingsystem/exceptions/ClaimNotValidException.java
+++ src/main/java/com/hexaware/medicalbillingsystem/exceptions/ClaimNotValidException.java
@@ -2,12 +2,14 @@ package com.hexaware.medicalbillingsystem.exceptions;
 
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
-
+/*
+@Author : Rajat Darvhekar  
+Modified Date : 15-10-2023
+Description : Getting the exception of ClaimNotValidException
+*/
 public class ClaimNotValidException extends ResponseStatusException {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public ClaimNotValidException(HttpStatusCode status, String reason) {
