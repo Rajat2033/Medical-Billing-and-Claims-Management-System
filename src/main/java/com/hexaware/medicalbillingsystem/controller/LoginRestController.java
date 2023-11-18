c2f327cf44fba1e9cb860fba4f35b0bf8def8efe
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-18 11:48:21 +0530

    Update LoginRestController.java

--- src/main/java/com/hexaware/medicalbillingsystem/controller/LoginRestController.java
+++ src/main/java/com/hexaware/medicalbillingsystem/controller/LoginRestController.java
@@ -2,7 +2,11 @@ package com.hexaware.medicalbillingsystem.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+@Author :  Hema Sree   
+Modified Date : 14-11-2023
+Description : Controller  for Login
+*/
 @RestController
 @RequestMapping("/api/v1/login")
 public class LoginRestController {
