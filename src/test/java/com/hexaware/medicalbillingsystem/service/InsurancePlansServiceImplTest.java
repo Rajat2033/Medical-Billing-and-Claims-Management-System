bd96082e699e491f4dbb74f9b4f8e6a2b559c6f3
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-19 14:27:16 +0530

    Update InsurancePlansServiceImplTest.java

--- src/test/java/com/hexaware/medicalbillingsystem/service/InsurancePlansServiceImplTest.java
+++ src/test/java/com/hexaware/medicalbillingsystem/service/InsurancePlansServiceImplTest.java
@@ -3,7 +3,11 @@ package com.hexaware.medicalbillingsystem.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+/*
+@Author : Hema Sree 
+Modified Date : 18-11-2023
+Description : ServiceImpTest class for the InsurancePlans
+*/
 class InsurancePlansServiceImplTest {
 
 	@Test
