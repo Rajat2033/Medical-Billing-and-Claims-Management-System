ec697574297ff257cb64e20035f8883c355b7225
Hema-Sree01 <148618724+Hema-Sree01@users.noreply.github.com>
2023-11-19 14:54:12 +0530

    Update IAdminMedicalService.java

--- src/main/java/com/hexaware/medicalbillingsystem/service/IAdminMedicalService.java
+++ src/main/java/com/hexaware/medicalbillingsystem/service/IAdminMedicalService.java
@@ -2,7 +2,11 @@ package com.hexaware.medicalbillingsystem.service;
 
 import com.hexaware.medicalbillingsystem.dto.AdminMedicalDTO;
 import com.hexaware.medicalbillingsystem.entities.AdminMedical;
-
+/*
+@Author:Rajat Darvehkar
+Date:18-11-2023
+Description: Interface of IAdminMedicalService
+*/	
 public interface IAdminMedicalService {
 	public AdminMedical insertNewAdmin(AdminMedicalDTO adminDTO);
 	public AdminMedical updateAdmin(AdminMedicalDTO adminDTO,int adminId);
